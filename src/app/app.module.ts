148142fc1bf591cfa647a5e55ac2108e7fdd88a9
singhcodex <coder69developer@gmail.com>
2023-07-07 15:03:31 +0200

    create interface and mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { IssueListComponent } from './issue-list/issue-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
