97ae3eec3da4757b9ba1c94deb922039f434740a
singhcodex <coder69developer@gmail.com>
2023-07-07 15:48:15 +0200

    report and issue with reactive form

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
   styleUrls: ['./issue-list.component.css']
 })
-export class IssueListComponent {
+export class IssueListComponent implements OnInit {
+
+  issues: Issue[] = [];
+  showReportIssue:boolean = false;
+  constructor(private issueService: IssuesService){}
+
+  ngOnInit(): void{
+    this.getIssues();
+ }
+
+ private getIssues(){
+  this.issues = this.issueService.getPendingIssues();
+ }
+
+ onCloseReport(){
+  this.showReportIssue = false;
+  this.getIssues();
+ }
 
 }
